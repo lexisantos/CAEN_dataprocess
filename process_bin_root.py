ab1d8d56cc409c27c68808b013385e92fd7b3866
Alexia Santos <lexisantosca@gmail.com>
2026-04-14 17:39:12 -0300

    Generalización de análisis
    *En los BIN y ROOT ya no se usa Energy desde un inicio para marcar el canal de energía, se usa directamente Energy_Ch, para facilitar el código más adelante.
    * Las calibraciones se aplican directamente sobre Energy_Ch, tanto en coincidencias (especificando cuál de los detectores es E o dE) como en los otros datasets.
    
    Se hace una función *run_data_bin* que integre todas las funciones hasta ahora creadas:
    * analyze_bin_root se vuelve un notebook para un caso en especial (léase para entender lo que hace cada función creada en process_bin_root.py)
    * Esta función me construye todos los DataFrames que necesito, a partir del BIN.
    
    Además,
    * Funciones separadas para crear gráficos para cada dataset. En el caso de las coincidencias 
    * Se agrega la opción de usar seaborn o plotly express
    * Permite comparar histogramas de dos calibraciones en energía distintas para el caso de coincidencias.
    
    Se integra todo esto en una única función run_all, dentro de analyze_bin_root.ipynb. Esta se puede modificar para quitar o agregar los datos que se precisen en cada run o experiencia.
    
    Se documenta:
    * run_data_bin
    
    A documentar:
    *apply_calibration_en

--- process_bin_root.py
+++ process_bin_root.py
@@ -1,18 +1,10 @@
 #import ROOT para manipular archivos root (conda activate {nombre del environment con root})
 import struct
-import glob, os, bisect
+import glob, os
 import pandas as pd
 import numpy as np
 import seaborn.objects as so
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from scipy.signal import find_peaks
-
-#%% General
-
-def filter_hist(df_hist, counts_min = 0, counts_max = np.inf, ch_min = 0, ch_max = np.inf, x_col = 'Energy_Ch'):
-    df_hist = df_hist[(df_hist[x_col] <= ch_max) & (df_hist[x_col] >= ch_min) & (df_hist.Counts >= counts_min) & (df_hist.Counts <= counts_max)]
-    return df_hist
 
 #%% Process .root
 def encuentraObjetos(rootfile, path="", dictKeys = {'Name':[], 'ClassName': [], 'Path': []}):    
@@ -50,27 +42,13 @@ def get_histograms(rootfile):
                                              'error': err})
     return dict_datos
 
-def hist_root(rootfile):
-    dict_histograms = get_histograms(rootfile)
-    keys = [key for key in list(dict_histograms.keys()) if key.startswith('/Energy/')]
-    nro_ch = [int(key.split('CH')[1][0]) for key in keys]
-    dfs = []
-
-    for key, ch in zip(keys, nro_ch):
-        df_prov = dict_histograms[key].copy()
-        df_prov['Channel'] = ch
-        dfs.append(df_prov)
-
-    dfROOT = pd.concat(dfs, ignore_index=True)
-    return dfROOT
-
 def hist_energy(rootfile, graph = True):
     dict_histogramas = get_histograms(rootfile)
     dict_energy_tiempo = {}
 
     for key, df in dict_histogramas.items():
         name = 'Energy' if 'Energy' in key else 'Tiempo'
-        if name == 'Energy' and graph:
+        if name == 'Energy':
             (
                 so.Plot(data = df, x = 'bins_center', y = 'counts')
                 .add(so.Dot())
@@ -81,7 +59,7 @@ def hist_energy(rootfile, graph = True):
         dict_energy_tiempo[name] = df.bins_center
 
     df_energy_tiempo = pd.DataFrame(dict_energy_tiempo)
-    df_energy_tiempo.Tiempo = df_energy_tiempo.Tiempo/1E12
+    df_energy_tiempo = df_energy_tiempo/1E12
 
     return df_energy_tiempo
 
@@ -93,40 +71,37 @@ def bin_to_df(filepath):
 
     df = pd.DataFrame([[*line] for line in iter_data], 
                        columns = ['Board', 'Channel', 'Timestamp', 'Energy', 'Flag'])
-    #df['Timestamp'] = df['Timestamp']/1E12
+    df['Timestamp'] = df['Timestamp']/1E12
     return df
 
-def hist_bin(df_bin):
-    df_hist = (df_bin.groupby(['Channel', 'Energy_Ch'], as_index=False)
-               .agg(
-                   Counts = ('Flag', "count")
-            )).reset_index(drop = True)
+def hist_bin(filepath):
+    df_bin = bin_to_df(filepath)
+    df_hist = (df_bin.groupby('Channel', 'Energy', as_index=False)
+               .agg(Counts = ('Flag', "count")))
     return df_hist
 
-def BIN_files_classifier(filepath, arr_run_BINs):
+def BIN_sanity(filepath):
+    runs_only_BIN = folders_has_both(filepath, nro_root=0, nro_BIN=1)
     empty_BINs = []
     with_more_ch = {}
     broken_BINs = []
-
-    for run in arr_run_BINs:
-        str_bin = filepath + run + "/RAW/*.BIN"
+    for run in runs_only_BIN:
+        str_bin = filepath + run +"/RAW/*.BIN"
         try:
             binpath = glob.glob(str_bin)[0]
             df_bin = bin_to_df(binpath)
-        except Exception:
+        except:
             broken_BINs.append(run)
-            continue
-
+            pass
         if df_bin.empty:
             empty_BINs.append(run)
         else:
-            n = df_bin.Channel.nunique()
-            with_more_ch.setdefault(n, []).append(run)
-    return broken_BINs, empty_BINs, with_more_ch
-
-def create_BIN_resumen(filepath):
-    runs_only_BIN = folders_has_both(filepath, nro_root=0, nro_BIN=1)
-    broken_BINs, empty_BINs, with_more_ch = BIN_files_classifier(filepath, runs_only_BIN)
+            n = len(df_bin.Channel.unique())
+            if n in with_more_ch:
+                with_more_ch[n] += [run]
+            else:
+                with_more_ch[n] = [run]
+    
     resumen = {
         'empty': empty_BINs,
         'broken': broken_BINs,   
@@ -135,22 +110,13 @@ def create_BIN_resumen(filepath):
         resumen[f'{ii}_ch'] = values
     return resumen
 
-def BIN_sanity(filepath, name_csv = 'data_BIN_sanity.csv'):
-    if os.path.isfile(filepath + name_csv):
-        df = pd.read_csv(filepath + name_csv)
-        resumen = {col: df[col].dropna().tolist() for col in df}
-    else:
-        resumen = create_BIN_resumen(filepath)
-        pd.DataFrame.from_dict(resumen, orient= 'index').transpose().to_csv(filepath + name_csv, index=False)
-    return resumen
-
 
 #%% Finding .BIN and .root archives
 
 def find_archives(path_origin, extension: str = ".BIN"):
     archives = glob.glob("**/RAW/*"+extension, root_dir= path_origin, recursive=True)
     df = (pd.DataFrame([path.split('/') for path in archives], columns = ['run', 'RAW', 'filename'])
-          .drop(['RAW'], axis = 1)
+          .drop(columns = ['RAW'], axis = 1)
           .groupby('run', as_index= False)
           .agg(counts = ("filename", "count"))
           .rename(columns = {'counts': f"{extension[1:]}_counts"}))
@@ -164,56 +130,10 @@ def info_BIN_ROOT(path_origin, save_csv = False):
         df_merged.to_csv(path_origin+f'info_archives.csv')
     return df_merged
 
-def folders_has_both(path_origin: str, nro_BIN: int = 1, nro_root: int = 1):
-    '''
-    Returns an array of strings.
-    '''
+def folders_has_both(path_origin, nro_BIN = 1, nro_root = 1):
     df_folder = info_BIN_ROOT(path_origin)
     runs_with_both = df_folder[(df_folder.root_counts >= nro_BIN) & (df_folder.BIN_counts >= nro_root)].run.values
     return runs_with_both
-
-#%% Calibration
-
-def calibration_ch_en(dfBIN_hist, limits_w = (10, 50), limits_h = (0, np.inf)):
-    picos = {}
-    for ch in dfBIN_hist.Channel.unique():
-        picos[ch] = find_peaks(dfBIN_hist[dfBIN_hist.Channel == ch].Counts.values, width = limits_w, height = limits_h)
-    return picos
-
-def apply_calibration_en(df, df_coef, col = 'Energy', move_old_to = '', new = 'Energy'):
-    if move_old_to != '':
-        df[move_old_to] = df[col].values
-    df[new] = df[col].astype(float)
-    coef_dict = df_coef.set_index('det').to_dict(orient='index')
-    for det in df['Channel'].unique():
-        coef = coef_dict[det]
-        df.loc[df['Channel'] == det, new] = (
-            df.loc[df['Channel'] == det, new]
-            .map(lambda x: coef['m'] * x + coef['b'])
-        )
-    return df
- 
-#%% Coincidences
-
-def find_coincidences(df_0, df_1, window_ps):
-    coincidences = []
-    for i, t in enumerate(df_0.Timestamp.values):
-        left = bisect.bisect_left(df_1.Timestamp.values, t - window_ps/2)
-        right = bisect.bisect_right(df_1.Timestamp.values, t + window_ps/2)
-        for j in range(left, right):
-            coincidences.append((i, j))
-    return coincidences
-
-def df_coincidences(dfBIN, det_E, det_dE, window_ns, Energy_col = ['Energy_ch', 'Energy']):    
-    dfBIN_dE = dfBIN[dfBIN.Channel == det_dE].sort_values(by = 'Timestamp')
-    dfBIN_E = dfBIN[dfBIN.Channel == det_E].sort_values(by = 'Timestamp')
-    
-    df_coinc = pd.DataFrame(find_coincidences(dfBIN_E, dfBIN_dE, window_ns*1E3), columns = ['i', 'j'])
-    for col in Energy_col:
-        df_coinc[f'{col}_E'] = dfBIN_E.iloc[df_coinc.i][col].values
-        df_coinc[f'{col}_dE'] = dfBIN_dE.iloc[df_coinc.j][col].values
-
-    return df_coinc
 
 #%% Saving Graphs
 
@@ -226,37 +146,6 @@ def create_folder(folder_name):
             print(f"Error creating folder: {e}")
     else:
         print(f"Folder '{folder_name}' already exists.")
-
-def graph_coincidences(df_coinc, nro_bins: int = 200, Energy_col = 'Energy', add_both = False):
-    E_arr = df_coinc[f'{Energy_col}_E'].values
-    if add_both:
-        dE_arr = df_coinc[f'{Energy_col}_dE'].values + E_arr
-        y_lab = f'(E + dE) [MeV]' 
-    else:
-        dE_arr = df_coinc[f'{Energy_col}_dE'].values
-        y_lab = f'dE [MeV]'
-    counts, x_edges, y_edges = np.histogram2d(
-        E_arr, 
-        dE_arr,
-        bins= nro_bins  
-    )
-
-    plt.pcolormesh(
-    x_edges,
-    y_edges,
-    counts.T,
-    cmap='viridis',
-    shading='auto',
-    norm = LogNorm(vmin=1, vmax=counts.max()) # or use counts_masked = np.ma.masked_where(counts == 0, counts) instead
-
-    )
-
-    plt.colorbar()
-    plt.xlabel(f'E [MeV]')
-    plt.ylabel(y_lab)
-    plt.show()
-    return counts, x_edges, y_edges
-
 
 def graph_hist(df_bin, hist_root_dict, folder, name = 'Figure', window = 100, save_fig = False):
     save_fig_in = '/Graficos_root_bin'
@@ -282,28 +171,3 @@ def graph_hist(df_bin, hist_root_dict, folder, name = 'Figure', window = 100, sa
         else:
             plt.show()
     plt.close()
-
-
-#%% Precindible
-
-'''
-#Barrido de ventana
-
-windows = np.arange(400, 750, 50)*1E3 #ns -> ps
-coincidences_ch0ch1 = {}
-
-for window in windows.astype(int):
-    coincidences_ch0ch1[str(window)] = find_coincidences(dfBIN_ch0, dfBIN_ch1, window)
-
-plt.figure()
-chosen_w = 0
-
-for key, value in coincidences_ch0ch1.items():
-    print('Con ventana de', float(key)/1E3, 'ns se tienen', len(value), 'coincidencias')
-    if len(value) <= min(len(dfBIN_ch0), len(dfBIN_ch1)) or float(chosen_w) == 0.:
-        chosen_w = key
-    plt.plot(float(key)/1E3, len(value), 'ko')
-
-plt.xlabel('Ventana [ns]')
-plt.ylabel('# Coincidencias')
-'''
